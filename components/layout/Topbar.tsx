990f0f7ed48f154f0beea88a3c840ad6241fdf5b
Hasan Coder 66 <hasanashraf874@gmail.com>
2024-12-10 19:31:18 +0500

    left sidebar done working on topbar

--- components/layout/Topbar.tsx
+++ components/layout/Topbar.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { navLinks } from "@/lib/constant"
+import { UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
 
 const Topbar = () => {
   return (
-    <div>Tr</div>
+    <div className="sticky top-0 z-20 w-full flex justify-between items-center px-8 py-4 shadow-xl bg-blue-2 ">
+      <Image src={"/logo.png"} alt="Logo" width={150} height={70} />
+
+<div className="flex flex-col gap-12">
+  {navLinks.map((link) => (
+    <Link
+      href={link.url}
+      key={link.label}
+      className="flex gap-4 text-body-medium"
+    >
+      {" "}
+      {link.icon} <p>{link.label}</p>
+    </Link>
+  ))}
+</div>
+
+<div className="flex items-center gap-4 text-body-medium">
+  <UserButton />
+  <p>Edit Profile</p>
+</div>
+    </div>
   )
 }
 
